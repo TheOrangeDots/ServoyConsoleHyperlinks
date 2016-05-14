2deb12035f1c1fdbf398bf2cbe0d364dfa6eb5b8
Paul Bakker <paul@paulgbakker.com>
2016-05-14 12:44:59 +0200

    First commit

---
+++ com.tod.eclipse.servoy.console.patternMatchers/src/com/tod/eclipse/servoy/console/patternMatchers/Startup.java
@@ -0,0 +1,11 @@
+package com.tod.eclipse.servoy.console.patternMatchers;
+
+import org.eclipse.ui.IStartup;
+
+public class Startup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// Force startup
+	}
+}
