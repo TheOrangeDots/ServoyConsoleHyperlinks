df79e0f94332fbcf769e45c113c63db13128c78c
Paul Bakker <paul@paulgbakker.com>
2016-05-18 09:25:26 +0200

    Fixes #5: Inherited elements selection not working

--- patternMatchers/src/com/tod/eclipse/servoy/console/patternMatchers/ElementHyperlink.java
+++ patternMatchers/src/com/tod/eclipse/servoy/console/patternMatchers/ElementHyperlink.java
@@ -1,33 +1,18 @@
 package com.tod.eclipse.servoy.console.patternMatchers;
 
-import java.util.HashMap;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.GraphicalViewer;
-import org.eclipse.gef.ui.parts.GraphicalEditor;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.console.IHyperlink;
-import org.eclipse.ui.ide.IGotoMarker;
-import org.eclipse.ui.part.MultiPageEditorPart;
 
 import com.servoy.eclipse.designer.editor.BaseVisualFormEditor;
 import com.servoy.eclipse.designer.editor.BaseVisualFormEditorDesignPage;
 import com.servoy.eclipse.model.ServoyModelFinder;
-import com.servoy.eclipse.model.repository.SolutionSerializer;
 import com.servoy.eclipse.ui.util.EditorUtil;
 import com.servoy.j2db.FlattenedSolution;
+import com.servoy.j2db.persistence.AbstractRepository;
 import com.servoy.j2db.persistence.BaseComponent;
 import com.servoy.j2db.persistence.Form;
 import com.servoy.j2db.persistence.IPersist;
 import com.servoy.j2db.persistence.IPersistVisitor;
-import com.servoy.j2db.util.Debug;
-import com.servoy.j2db.util.Pair;
 
 public class ElementHyperlink implements IHyperlink {
 
@@ -64,8 +49,8 @@ public class ElementHyperlink implements IHyperlink {
 		if (this.element == null) {
 			return;
 		}
-
-		IPersist persist = (IPersist) fs.getFlattenedForm(f).acceptVisitor(new IPersistVisitor() {
+		
+			IPersist persist = (IPersist) fs.getFlattenedForm(f).acceptVisitor(new IPersistVisitor() {
 			public Object visit(IPersist o) {
 				if (o instanceof BaseComponent) {
 					BaseComponent bc = (BaseComponent) o;
@@ -82,44 +67,6 @@ public class ElementHyperlink implements IHyperlink {
 		}
 	
 		BaseVisualFormEditor fe = (BaseVisualFormEditor) ep;
-		((BaseVisualFormEditorDesignPage)fe.getGraphicaleditor()).showPersist(persist);
-
-		//Better alternative that doesn't depend on including Servoy specific classes, but it doesn't work...
-//		GraphicalViewer gv = (GraphicalViewer) ((GraphicalEditor)ep).getAdapter(GraphicalViewer.class);
-//		Object editPart = gv.getRootEditPart().getViewer().getEditPartRegistry().get(persist);
-//		
-//		if (editPart instanceof EditPart) {
-//			// select the marked element
-//			gv.setSelection(new StructuredSelection(editPart));
-//			gv.reveal((EditPart)editPart);
-//		}
-		
-		
-		
-//		IGotoMarker gm = (IGotoMarker) fe.getAdapter(IGotoMarker.class);
-//		
-//		IMarker marker = null;
-//		try {
-//			Pair<String, String> pathPair = SolutionSerializer.getFilePath(persist, true);
-//			Path path = new Path(pathPair.getLeft() + pathPair.getRight());
-//			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
-//			if (file.exists()) {
-//				marker = file.createMarker("com.tod.console.hyperlink2selection.helpermarker");
-//				HashMap<String, String> attributes = new HashMap<String, String>();
-//				attributes.put("elementUuid", persist.getUUID().toString());
-//				marker.setAttributes(attributes);
-//				//IDE.gotoMarker(ep, marker);
-//				gm.gotoMarker(marker);
-//			}
-//		} catch (CoreException e) {
-//			Debug.error("Failure selecting element", e);
-//		} finally {
-//			if (marker != null)
-//				try {
-//					marker.delete();
-//				} catch (CoreException e) {
-//					// ignore
-//				}
-//		}
+		((BaseVisualFormEditorDesignPage)fe.getGraphicaleditor()).showPersist(AbstractRepository.searchPersist(f, persist.getUUID()));
 	}
 }
