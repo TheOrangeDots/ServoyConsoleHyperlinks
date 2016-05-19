f39532970d87e523f14827e019de3ac62bf6af05
Paul Bakker <paul@paulgbakker.com>
2016-05-19 13:23:59 +0200

    Fixes #10: Support element selection in Servoy 8

--- patternMatchers/src/com/tod/eclipse/servoy/console/hyperlinks/Hyperlink.java
+++ patternMatchers/src/com/tod/eclipse/servoy/console/hyperlinks/Hyperlink.java
@@ -1,11 +1,18 @@
 package com.tod.eclipse.servoy.console.hyperlinks;
 
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.console.IHyperlink;
+import org.eclipse.ui.ide.IGotoMarker;
 
-import com.servoy.eclipse.designer.editor.BaseVisualFormEditor;
-import com.servoy.eclipse.designer.editor.BaseVisualFormEditorDesignPage;
 import com.servoy.eclipse.model.ServoyModelFinder;
+import com.servoy.eclipse.model.repository.SolutionSerializer;
 import com.servoy.eclipse.ui.util.EditorUtil;
 import com.servoy.j2db.FlattenedSolution;
 import com.servoy.j2db.persistence.AbstractRepository;
@@ -13,6 +20,8 @@ import com.servoy.j2db.persistence.BaseComponent;
 import com.servoy.j2db.persistence.Form;
 import com.servoy.j2db.persistence.IPersist;
 import com.servoy.j2db.persistence.IPersistVisitor;
+import com.servoy.j2db.util.Debug;
+import com.servoy.j2db.util.Pair;
 import com.servoy.j2db.util.UUID;
 
 public class Hyperlink implements IHyperlink {
@@ -76,7 +85,30 @@ public class Hyperlink implements IHyperlink {
 			return;
 		}
 		
-		BaseVisualFormEditor fe = (BaseVisualFormEditor) ep;
-		((BaseVisualFormEditorDesignPage)fe.getGraphicaleditor()).showPersist(AbstractRepository.searchPersist(f, persistUUID));
+		IPersist persist = AbstractRepository.searchPersist(f, persistUUID);
+		IGotoMarker gm = (IGotoMarker) ep.getAdapter(IGotoMarker.class);
+		
+		IMarker marker = null;
+		try {
+			Pair<String, String> pathPair = SolutionSerializer.getFilePath(persist, true);
+			Path path = new Path(pathPair.getLeft() + pathPair.getRight());
+			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+			if (file.exists()) {
+				marker = file.createMarker("com.tod.console.hyperlink2selection.helpermarker");
+				HashMap<String, String> attributes = new HashMap<String, String>();
+				attributes.put("elementUuid", persist.getUUID().toString());
+				marker.setAttributes(attributes);
+				gm.gotoMarker(marker);
+			}
+		} catch (CoreException e) {
+			Debug.error("Failure selecting element", e);
+		} finally {
+			if (marker != null)
+				try {
+					marker.delete();
+				} catch (CoreException e) {
+					// ignore
+				}
+		}
 	}
 }
